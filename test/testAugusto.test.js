10d22b195732e048edbe6bf3e29a935af696fa1c
AugustoComeglio <augustoc.98@gmail.com>
2021-10-07 16:17:37 -0300

    Test unitarios Augusto

--- test/testAugusto.test.js
+++ test/testAugusto.test.js
@@ -1,3 +1,5 @@
+//EJEMPLO JEST
+/*
 const { it } = require('@jest/globals');
 const { test } = require('picomatch');
 const sum = require('../src/codigo.js');
@@ -10,3 +12,25 @@ describe('Pruebas para sumas', () => {
         expect(sum(1,5)).toBe(6);    
     });
 })
+*/
+const { it } = require('@jest/globals');
+//Tst 1
+const getPrecio = require('../src/codigo.js');
+describe('Prueba precio distinto de 0',() => {
+    it('Demostrar precio distinto de 0', () => {
+        expect(getPrecio).not.toBe(0);
+    });
+});
+
+//Test 2
+const carrito1 = require('../src/codigo.js');
+describe('Prueba carrito no vacio',() => {
+    it('Demostrar que el carrito no esta vacio', () => {
+        expect(carrito1).not.toBe(0);
+    });
+});
+
+
+
+
+
